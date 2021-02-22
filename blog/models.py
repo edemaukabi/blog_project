43c0e53ecfba5ba58697077d692e7646e386b442
edemaukabi <edemaukabi@gmail.com>
2021-02-22 22:44:17 +0100

    blog projct

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.urls import reverse
-# Create your models here
+# Create your models
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
